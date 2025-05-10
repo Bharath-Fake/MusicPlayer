4f9dafe7b30720de8c5b77127fea93d725878386
Bharath-Fake <justdofakethings@gmail.com>
2025-05-10 18:11:17 +0530

    Adjusted VITE API URL

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin:import.meta.env.VITE_API_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
