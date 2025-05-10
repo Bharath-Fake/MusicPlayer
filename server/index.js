c417ea511b44f678db3ad8f3b87da08cdc5bf701
Bharath-Fake <justdofakethings@gmail.com>
2025-05-10 18:16:15 +0530

    Adjusted origin

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:import.meta.env.VITE_API_URL,
+  origin:"https://musicplayer-1jh8.onrender.com/api",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
